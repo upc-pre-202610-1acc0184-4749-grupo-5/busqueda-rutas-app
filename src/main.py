be0a6f09d1e54c31734061896fa2db5994d4d224
Kobashigawa Rio <u202412921@upc.edu.pe>
2026-09-21 06:00:24 +0900

    feat(main): Import PyQT5 Library to main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import PyQt5
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QVBoxLayout, QTextEdit, QComboBox, QDialog, QLineEdit, QMessageBox
+
+# App Info
+AppName = "Busqueda Rutas App"
+AppDescription = " "
+version = "1.0.0"
+
