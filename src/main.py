a8e17a78c34f8680ac27e78515c20b5d385f402f
Kobashigawa Rio <u202412921@upc.edu.pe>
2026-09-21 06:50:46 +0900

    feat(main): Initialize Main Window in main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import PyQt5
+import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QVBoxLayout, QTextEdit, QComboBox, QDialog, QLineEdit, QMessageBox
 
 # App Info
@@ -6,3 +7,23 @@ AppName = "Busqueda Rutas App"
 AppDescription = " "
 version = "1.0.0"
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(AppName + " ver" + version)
+        self.setWindowOpacity(0.95)
+        self.setGeometry(700,700,500,500)
+
+# Initialize GUI
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    app.setApplicationName(AppName)
+    app.setApplicationDisplayName(AppName)
+    app.setOrganizationName("Group 6")
+
+    app.setDesktopFileName(AppName)
+
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
